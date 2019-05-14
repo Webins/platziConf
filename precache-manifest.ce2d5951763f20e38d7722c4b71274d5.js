7b27254f7d740f6bc7736b729b406e25974de343
Webins <kleivermarcano@hotmail.com>
2019-05-13 20:35:36 -0400

    Updates

---
+++ precache-manifest.ce2d5951763f20e38d7722c4b71274d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787fb7931e062b9efe791bb0b7042669",
+    "url": "/platziConf/index.html"
+  },
+  {
+    "revision": "c7ae04912f38aecc0e73",
+    "url": "/platziConf/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "20c800bee7c05fc4f380",
+    "url": "/platziConf/static/css/main.de458e2f.chunk.css"
+  },
+  {
+    "revision": "c7ae04912f38aecc0e73",
+    "url": "/platziConf/static/js/2.e6a383ef.chunk.js"
+  },
+  {
+    "revision": "20c800bee7c05fc4f380",
+    "url": "/platziConf/static/js/main.6184253a.chunk.js"
+  },
+  {
+    "revision": "bcd477eb5604d7c45d3a",
+    "url": "/platziConf/static/js/runtime~main.d9a22632.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/platziConf/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/platziConf/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platziConf/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platziConf/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platziConf/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "45037fdb64ceedfb1645dc6a0dc365b0",
+    "url": "/platziConf/static/media/twitter.45037fdb.svg"
+  }
+]);
--- src/components/PageLoading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Loader from './Loader';
-function PageLoading(){
-
-    return(<Loader/>)
-}
-
-export default PageLoading;
